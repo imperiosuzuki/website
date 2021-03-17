28f75105e1ca28a03daf9515af1ba5cc00e931a9
unknown <pedropaulosuzuki1@gmail.com>
2021-03-17 15:00:33 -0300

    Updated visuals.

---
+++ docs/scripts/script.js
@@ -0,0 +1,4 @@
+//
+//
+
+alert('Seja bem vindo ao nosso website!\n\nEstamos em transição no momento. Para nos contactar, ligue para +55 11 3975-0000, ou através do email imperio.suzuki1@gmail.com.')
